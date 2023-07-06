cfd2517c01b0c268dd8645270acf9503eb510499
Nishima Sitaula <nishimasitaula1@gmail.com>
2023-07-06 09:57:42 +0545

    Dependency Injection Example

---
+++ src/cpu/cpu.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { CpuService } from './cpu.service';
+import { PowerModule } from 'src/power/power.module';
+import { DiskService } from 'src/disk/disk.service';
+
+@Module({
+  imports: [PowerModule],
+  exports: [CpuService],
+  providers: [CpuService]
+})
+export class CpuModule {}
